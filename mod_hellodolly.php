50359146360738ab8392a35747feb2a1b754c57e
Travis Weston <tweston2013@gmail.com>
2020-12-14 18:06:45 -0500

    Split WP and Joomla loaders again because Joomla doesn't like that, and it isn't really necessary

---
+++ mod_hellodolly.php
@@ -0,0 +1,18 @@
+<?php
+
+if( defined('_JEXEC') ){
+
+    require_once( __DIR__ . '/HelloDolly/hello.php' );
+    
+    $texturizer = function($line) {
+        return $line;
+    };
+    
+    $translator = function($line) {
+        return $line;
+    };
+    
+    $hello = new \HelloDolly\HelloDolly($texturizer, $translator);
+    require JModuleHelper::getLayoutPath('mod_hellodolly');
+
+}
--- wp-loader.php
+++ wp-loader.php
@@ -21,20 +21,3 @@ if(defined('WPINC')){
     (new \WordPress\DisplayDriver($hello))->register();
     
 }
-
-if( defined('_JEXEC') ){
-
-    require_once( __DIR__ . '/HelloDolly/hello.php' );
-    
-    $texturizer = function($line) {
-        return $line;
-    };
-    
-    $translator = function($line) {
-        return $line;
-    };
-    
-    $hello = new \HelloDolly\HelloDolly($texturizer, $translator);
-    require JModuleHelper::getLayoutPath('mod_hellodolly');
-
-}
